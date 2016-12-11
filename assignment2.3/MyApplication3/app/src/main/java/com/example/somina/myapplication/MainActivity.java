414bc55c33102a0007e795c0541470622cba0b46
Subhrajyoti123 <subhra.brahma9@gmail.com>
2016-12-11 14:01:52 +0530

    assignment2.3
    LoginScreen

---
+++ assignment2.3/MyApplication3/app/src/main/java/com/example/somina/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.somina.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
